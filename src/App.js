f0a20379155fa77ec841459d3b131e915be61f88
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-07-27 17:03:32 +0500

    removed context api

--- src/App.js
+++ src/App.js
@@ -5,25 +5,23 @@ import Foreground1 from './components/Foreground1'
 import Foreground2 from './components/Foreground2'
 import Background1 from './components/Background1'
 import Background2 from './components/Background2'
-import { ContextProvider } from './store/context'
+
 
 
 
 function App() {
 
     return (
-        <ContextProvider>
-            <div className="wrapper">
-                <div className="sky"></div>
-                <div className="earth">
-                    <AliceQueenSprite />
-                </div>
-                <Foreground1 />
-                <Foreground2 />
-                <Background1 />
-                <Background2 />
+        <div className="wrapper">
+            <div className="sky"></div>
+            <div className="earth">
+                <AliceQueenSprite />
             </div>
-        </ContextProvider>
+            <Foreground1 />
+            <Foreground2 />
+            <Background1 />
+            <Background2 />
+        </div>
     )
 }
 
